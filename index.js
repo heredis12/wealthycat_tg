de3fd4ae9bbd13584c537e57de9c850b5c74f267
heredis12 <32660179+heredis12@users.noreply.github.com>
2024-07-03 10:40:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { Telegraf, Markup } = require('telegraf')
 
-const token = "7403988653:AAG-rRgaKUiXPzqy40OcBm8vHCQjjScPJSE";
+const token = "7247649430:AAGq1xQz2uebcReb4Rx4KpGDbQffPtrQJcA";
 if (token === undefined) {
   throw new Error('BOT_TOKEN must be provided!')
 }
 
-const gameShortName = 'wealthycat_tg'
+const gameShortName = 'wealthycat'
 const gameUrl = 'https://github.com/heredis12/wealthycat_tg'
 
 const markup = Markup.inlineKeyboard([
@@ -13,19 +13,13 @@ const markup = Markup.inlineKeyboard([
   Markup.button.url('Telegraf help', 'http://telegraf.js.org')
 ])
 
-bot.on(message('text'), async (ctx) => {
-    // Explicit usage
-    await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
-  
-    // Using context shortcut
-    await ctx.reply(`Hello ${ctx.state.role}`)
-  })
-
 const bot = new Telegraf(token)
+
 bot.start((ctx) => ctx.replyWithGame(gameShortName))
 bot.command('foo', (ctx) => ctx.replyWithGame(gameShortName, markup))
 bot.gameQuery((ctx) => ctx.answerGameQuery(gameUrl))
 bot.launch()
+
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
