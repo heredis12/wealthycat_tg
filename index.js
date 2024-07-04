2e281c93a825b64809ebb572e35f92da8cadab9a
heredis12 <32660179+heredis12@users.noreply.github.com>
2024-07-04 20:58:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-const { Telegraf, Markup } = require('telegraf')
-
-const token = "7247649430:AAGq1xQz2uebcReb4Rx4KpGDbQffPtrQJcA";
-if (token === undefined) {
-  throw new Error('BOT_TOKEN must be provided!')
-}
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
 
 const gameShortName = 'wealthycat'
 const gameUrl = 'https://heredis12.github.io/wealthycat_tg/'
 
-const markup = Markup.inlineKeyboard([
-  Markup.button.game('🎮 Play now!'),
-  Markup.button.url('Telegraf help', 'https://telegraf.js.org')
-])
-
-const bot = new Telegraf(token)
-
+const bot = new Telegraf("7247649430:AAGq1xQz2uebcReb4Rx4KpGDbQffPtrQJcA")
 bot.start((ctx) => ctx.replyWithGame(gameShortName))
-bot.command('foo', (ctx) => ctx.replyWithGame(gameShortName, markup))
 bot.gameQuery((ctx) => ctx.answerGameQuery(gameUrl))
-bot.launch()
 
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+
+
+// const markup = Markup.inlineKeyboard([
+//   Markup.button.game('🎮 Play now!'),
+//   Markup.button.url('Telegraf help', 'https://telegraf.js.org')
+// ])
+
+// const bot = new Telegraf(token)
+
+// bot.start((ctx) => ctx.replyWithGame(gameShortName))
+// bot.command('foo', (ctx) => ctx.replyWithGame(gameShortName, markup))
+// bot.gameQuery((ctx) => ctx.answerGameQuery(gameUrl))
+// bot.launch()
+
+
+// // Enable graceful stop
+// process.once('SIGINT', () => bot.stop('SIGINT'))
+// process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
 //Sharing link
 //Saving player info
